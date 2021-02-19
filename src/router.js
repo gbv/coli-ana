ada4b0a64f1288d92aa90270796c280a50015405
Stefan Peters <stefandesu@exo.pm>
2021-02-19 11:43:02 +0100

    Fix double / for URLs in vue-router

--- src/router.js
+++ src/router.js
@@ -15,11 +15,16 @@ const routes = [
   },
 ]
 
+let base = import.meta.env.BASE_URL
+if (base.endsWith("/")) {
+  base = base.substring(0, base.length - 1)
+}
+
 export function createRouter() {
   return _createRouter({
     // use appropriate history implementation for server/client
     // import.meta.env.SSR is injected by Vite.
-    history: import.meta.env.SSR ? createMemoryHistory(import.meta.env.BASE_URL) : createWebHistory(import.meta.env.BASE_URL),
+    history: import.meta.env.SSR ? createMemoryHistory(base) : createWebHistory(base),
     routes,
   })
 }
