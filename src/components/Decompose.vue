0f22724c759e4ea90ceafba09c4efabac88c6836
Stefan Peters <stefandesu@exo.pm>
2021-03-05 13:23:09 +0100

    Fix highlighted notation part

--- src/components/Decompose.vue
+++ src/components/Decompose.vue
@@ -165,7 +165,7 @@ export default {
         if (matches.length < 4) {
           return notation
         }
-        return `${notation.slice(0, matches[1].length)}<span class="highlightedNotationPart">${notation.slice(matches[1].length, matches[1].length + matches[2].length)}</span>${notation.slice(matches[1].length + matches[2].length)}`
+        return `${notation.slice(0, matches[1].length)}<span style="background-color: #E9E1E1;">${notation.slice(matches[1].length, matches[1].length + matches[2].length)}</span>${notation.slice(matches[1].length + matches[2].length)}`
       },
     }
   },
@@ -196,8 +196,5 @@ export default {
 }
 h4 {
   margin-top: 30px;
-}
-.highlightedNotationPart {
-  background-color: #E9E1E1;
 }
 </style>
