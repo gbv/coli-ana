f747863e793cbaeccf3d64b01758a0015d86a952
Stefan Peters <stefandesu@exo.pm>
2021-03-08 09:25:58 +0100

    Lint fix ConceptDetails.vue

--- src/components/ConceptDetails.vue
+++ src/components/ConceptDetails.vue
@@ -5,7 +5,10 @@
   </p>
   <p>
     {{ concept.uri }}
-    <a :href="'https://coli-conc.gbv.de/cocoda/app/?fromScheme=http%3A%2F%2Fdewey.info%2Fscheme%2Fedition%2Fe23%2F&from='+concept.uri" title="Cocoda" target="cocoda">&nesear;</a>
+    <a
+      :href="'https://coli-conc.gbv.de/cocoda/app/?fromScheme=http%3A%2F%2Fdewey.info%2Fscheme%2Fedition%2Fe23%2F&from='+concept.uri"
+      title="Cocoda"
+      target="cocoda">&nesear;</a>
   </p>
 </template>
 
