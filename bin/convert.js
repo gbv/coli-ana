094c7ad4dfbfbb66f6eb09f3f7bef14bd4a728f5
Stefan Peters <Stefan.Peters@gbv.de>
2021-06-07 11:01:54 +0200

    Refactor convert.js (#33)

--- bin/convert.js
+++ bin/convert.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
 import fs from "fs"
-import readline from "readline"
 import prisma from "../lib/prisma.js"
-import ddc from "../config/ddc.js"
 import { picaFromDDC } from "../lib/pica.js"
+import { parseInputStream } from "../lib/parseInputStream.js"
 
 // Basic argument parsing
 const args = process.argv.slice(2)
@@ -40,12 +39,6 @@ const warn = (...args) => {
   shouldImport && !quiet && console.warn(...args)
 }
 
-// Regular expressions
-const startRe = /^([0-9.-]+) \(\S*\)/
-const facetIndicatorRe = /^(\S*) <(Facet Indicator)> \(notation: (.*)\)/
-const lineRe = /^(\S*) (.*) \(notation: (.*)\)/
-const endRe = /^\s*$/
-
 if (!files.length) {
   console.error("Error: Please provide input file(s).")
   process.exit(1)
@@ -66,107 +59,55 @@ files.forEach(file => {
   }
 
   let result = []
-  let current = null
   let totalAdded = 0
-  let currentHasError = false
   let totalErrors = 0
 
-  async function end(forceProcess = false) {
-    if (current) {
-      result.push(current)
-    }
-    current = null
-    // Process every 50k results
-    if (forceProcess || result.length >= 50000) {
-      if (shouldImport) {
-        const createMany = await prisma.data.createMany({
+  async function processResults() {
+    if (shouldImport) {
+      const transaction = await prisma.$transaction([
+        prisma.data.deleteMany({
+          where: {
+            uri: {
+              in: result.map(r => r.uri),
+            },
+          },
+        }),
+        prisma.data.createMany({
           data: result,
           skipDuplicates: true,
-        })
-        log(`Added ${createMany.count} records.`)
-        totalAdded += createMany.count
-        result = []
-      } else {
-        result.forEach(item => {
-          if (picaFormat) {
-            item = picaFromDDC(item)
-          }
-          console.log(JSON.stringify(item))
-        })
-        result = []
-      }
+        }),
+      ])
+      const count = transaction[1].count
+      log(`Added ${count} records.`)
+      totalAdded += count
+      result = []
+    } else {
+      result.forEach(item => {
+        if (picaFormat) {
+          item = picaFromDDC(item)
+        }
+        console.log(JSON.stringify(item))
+      })
+      result = []
     }
   }
 
   for (let file of files) {
     log(`Reading file ${file}...`)
     const fileStream = fs.createReadStream(file)
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity,
-    })
-    for await (const line of rl) {
-      const startMatch = startRe.exec(line)
-      const facetIndicatorMatch = facetIndicatorRe.exec(line)
-      const lineMatch = lineRe.exec(line)
-      const endMatch = endRe.exec(line)
-
-      if (startMatch) {
-        const notation = startMatch[1]
-        current = ddc.conceptFromNotation(notation, { inScheme: true })
-        current.memberList = []
-        currentHasError = false
-      } else if (!current || (!ignoreErrors && currentHasError)) {
-        // TODO: How to deal with errors like this?
-        // It happens for example in ou_gvk_all18_3c_de_slim-21-05-01 (but where?)
-      } else if (endMatch) {
-        await end()
-      } else if (facetIndicatorMatch) {
-        // Special concept for facet indicator
-        current.memberList.push({
-          uri: `http://dewey.info/facet/${facetIndicatorMatch[3]}`,
-          notation: [facetIndicatorMatch[3], facetIndicatorMatch[1]],
-          prefLabel: { en: "facet indicator", de: "Facettenindikator" },
-        })
-      } else if (lineMatch) {
-        let prefLabel = lineMatch[2]
-        let notation = lineMatch[3]
-        // Deal with #dno_span_cen#
-        // TODO: Remove this after exported data is fixed.
-        if (prefLabel.endsWith("#dno_span_cen#")) {
-          prefLabel = prefLabel.replace(" #dno_span_cen#", "")
-          // Also remove :xyz from notation
-          notation = notation.replace(/:.*/, "")
-        }
-        const member = ddc.conceptFromNotation(notation)
-        if (member) {
-          member.prefLabel = { de: prefLabel }
-          member.notation.push(lineMatch[1])
-          // Some error checks (#31)
-          // 1. Check length of notation
-          if (current.notation[0].length !== member.notation[1].length) {
-            warn(`Error: Second notations should have the same length as analyzed notation, skipping. (${current.notation[0]})`)
-            !currentHasError && (totalErrors += 1)
-            currentHasError = true
-          }
-          // 2. Check for unexpected duplicates
-          if (current.memberList.find((m) => m.notation.join(" ") === member.notation.join(" "))) {
-            warn(`Error: Unexpected duplicate in analysis, skipping. (${current.notation[0]})`)
-            !currentHasError && (totalErrors += 1)
-            currentHasError = true
-          }
-          // Add member to memberList
-          current.memberList.push(member)
-        } else {
-          warn(`Warning: Could not convert DDC notation ${lineMatch[3]}`)
+    for await (const concept of parseInputStream(fileStream, { log, warn, ignoreErrors })) {
+      if (concept) {
+        result.push(concept)
+        // Process in batches of 25k results
+        if (result.length >= 25000) {
+          await processResults()
         }
       } else {
-        warn(`Warning: Could not parse line ${line}`)
+        totalErrors += 1
       }
     }
-    await end()
   }
-  await end(true)
+  await processResults()
   log(`Import completed. Added ${totalAdded} records in total. ${totalErrors} records had an error and were ${ignoreErrors ? "not " : ""}skipped${ignoreErrors ? " (--ignore-errors was given)" : ""}.`)
 
   await prisma.$disconnect()
