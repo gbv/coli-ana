a44da68767f8a759057ed801f77f0207bcfda8b6
Jakob Voss <voss@gbv.de>
2021-11-24 14:43:05 +0100

    Add PICA tests

--- test/pica.js
+++ test/pica.js
@@ -1,18 +1,15 @@
 import { expect } from "chai"
 import { picaFromDDC, serializePicaField } from "../lib/pica.js"
 
-var memberList = [
-  { notation: ["2","2----"] },
-  { notation: ["28","28---"] },
-  { notation: ["282","282--"] },
-  { notation: ["282.4-282.9","282.5"] },
-  { notation: ["T2--4-9","---.5"] },
-  { notation: ["T2--5","---.5"] },
-]
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
+const tests = require("./pica-tests.json")
 
-describe("picaFromDDC", () => {
-  it("should detect base number", () => {
-    const pica = picaFromDDC({ memberList, notation: ["282.5"] })
-    expect(serializePicaField(pica[0])).equal("045H/20 $eDDC23ger$a282.5$c282$g5$Acoli-ana")
+describe("serializePicaField", () => {
+  tests.forEach(({notation, memberList, pica}) => {
+    it(`${notation}`, () => {
+      const field = picaFromDDC({ notation, memberList })[0]
+      expect(serializePicaField(field)).equal(pica)
+    })
   })
 })
