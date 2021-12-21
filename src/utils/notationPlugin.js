a11c964bd75ca387858b873f0011a20a16fb55ac
Stefan Peters <Stefan.Peters@gbv.de>
2021-12-21 08:44:49 +0100

    notationPlugin: Fix display of notations for table ranges

--- src/utils/notationPlugin.js
+++ src/utils/notationPlugin.js
@@ -1,8 +1,14 @@
 import jskos from "jskos-tools"
 
 export default (notation, { item }) => {
-  let fill = ""
+
+  // Fix notations for table ranges
+  if (notation) {
+    notation = notation.replace(/^(T[1-9][A-Z]?--)(.*)-(.*)$/, "$1$2-$1$3")
+  }
+
   // For DDC and SDNB only: fill number notation with trailing zeros
+  let fill = ""
   if (jskos.compare({
     uri: "http://dewey.info/scheme/edition/e23/",
     identifier: [
@@ -21,5 +27,6 @@ export default (notation, { item }) => {
     // Using the shared `jskos-vue-text-lightGrey` CSS class
     notation += `<span class='jskos-vue-text-lightGrey'>${fill}</span>`
   }
+
   return notation
 }
