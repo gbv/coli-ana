26b3d8c139aa07b0487193e0a05686e83a2199f9
Stefan Peters <Stefan.Peters@gbv.de>
2021-12-22 11:29:37 +0100

    Adjust notationPlugin

--- src/utils/notationPlugin.js
+++ src/utils/notationPlugin.js
@@ -1,13 +1,7 @@
 import jskos from "jskos-tools"
 
 export default (notation, { item }) => {
-
-  // Fix notations for table ranges
-  if (notation) {
-    notation = notation.replace(/^(T[1-9][A-Z]?--)(.*)-(.*)$/, "$1$2-$1$3")
-  }
-
-  // For DDC and SDNB only: fill number notation with trailing zeros
+  // Special handling for DDC and SDNB
   let fill = ""
   if (jskos.compare({
     uri: "http://dewey.info/scheme/edition/e23/",
@@ -18,9 +12,16 @@ export default (notation, { item }) => {
       "http://id.loc.gov/vocabulary/classSchemes/sdnb",
       "http://uri.gbv.de/terminology/sdnb",
     ],
-  }, item.inScheme && item.inScheme[0]) && !isNaN(parseInt(notation))) {
-    while (notation.length + fill.length < 3) {
-      fill += "0"
+  }, item.inScheme && item.inScheme[0])) {
+    // Fix notations for table ranges
+    if (notation) {
+      notation = notation.replace(/^(T[1-9][A-Z]?--)(.*)-(.*)$/, "$1$2-$1$3")
+    }
+    // Fill number notation with trailing zeros
+    if (!isNaN(parseInt(notation))) {
+      while (notation.length + fill.length < 3) {
+        fill += "0"
+      }
     }
   }
   if (fill.length) {
