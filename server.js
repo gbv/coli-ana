2fbc3383a66ca7c0833e66508b1f785ccd3ce3a2
Jakob Voss <voss@gbv.de>
2022-01-04 14:24:13 +0100

    Remove isMemberParentOf from index.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ import express from "express"
 import config from "./config/index.js"
 import compression from "compression"
 import serveStatic from "serve-static"
-import { decomposeDDC, findMembers, isMemberParentOf } from "./lib/index.js"
+import { decomposeDDC, findMembers  } from "./lib/index.js"
+import isMemberParentOf from "./lib/isMemberParentOf.js"
 import { serializePica, picaFromDDC, pica3FromDDC } from "./lib/pica.js"
 const { ddc } = config
 
