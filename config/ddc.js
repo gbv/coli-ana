90f64be6df178392d4b2dafbbcf4c46cd170716d
Stefan Peters <Stefan.Peters@gbv.de>
2024-01-25 15:23:54 +0100

    Fix DDC notationPattern

--- config/ddc.js
+++ config/ddc.js
@@ -3,7 +3,7 @@ import jskos from "jskos-tools"
 const ddc = new jskos.ConceptScheme({
   uri: "http://dewey.info/scheme/edition/e23/",
   uriPattern: "^http://dewey.info/class/(.+)/e23/$",
-  notationPattern: "[0-9][0-9]?|[0-9]{3}(-[0-9]{3})?|[0-9]{3}.[0-9]+(-[0-9]{3}.[0-9]+(:[0-9]+)?)?|T?[1-9][A-Z]?--[0-9]+|[1-9][A-Z]?--[0-9]+(-[1-9][A-Z]?--[0-9]+(:[0-9]+)?)?",
+  notationPattern: "(([0-9][0-9]?|[0-9]{3}(-[0-9]{3})?|[0-9]{3}\\.[0-9]+(-[0-9]{3}\\.[0-9]+(:[0-9]+)?)?|T[1-9][A-Z]?--[0-9]+(-T[1-9][A-Z]?--[0-9]+(:[0-9]+)?)?|[1-9][A-Z]?--[0-9]+(-[1-9][A-Z]?--[0-9]+(:[0-9]+)?)?)?)",
 })
 
 // DDC table numbers start with a 'T' but this letter is omitted in the URI.
